d09d7920894e6c3cd5d1ac3cfffc6e0345b3bfbe
EthanGuo <spritegzq@gmail.com>
2013-09-04 10:11:47 +0800

    commit

--- server/smartserver/v2/impl/connector.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from mongoengine import *
-
-class connector(object):
-    def __init__(self):
-        connect('smartServer-refactor', host='192.168.5.60', port=27017)
