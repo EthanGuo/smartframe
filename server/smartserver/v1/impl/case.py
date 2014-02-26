bafc7baf9ca7597033a15cfa93599967256eb8b1
Ethan <spritegzq@gmail.com>
2014-02-26 14:21:22 +0800

    bug fix

--- server/smartserver/v1/impl/case.py
+++ server/smartserver/v1/impl/case.py
@@ -95,7 +95,7 @@ def __caseresultInsert(data, sid):
 def __updateCaseResult(data, sid):
     case = Cases.objects(sid=sid, tid=data['tid']).only('result', 'comments').first()
     if not case:
-        __caseresultInsert()
+        __caseresultInsert(data, sid)
     orgresult = case.result
     if case.comments and case.comments.caseresult:
         orgcommentresult = case.comments.caseresult
